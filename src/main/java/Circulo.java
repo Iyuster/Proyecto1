8dee6601958ed47d25bb34642f3682edcd989fb5
2024-25 DAM1 <2024-25 DAM1@DESKTOP-Q2BTK0C>
2024-10-14 17:34:00 +0200

    Primer commit

---
+++ src/main/java/Circulo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 2024-25 DAM1
+ */
+public class Circulo {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+    
+    
+    
+}
